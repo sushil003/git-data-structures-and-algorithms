d7f26b784a7e33f7a2798eba9845d36c65c7e79e
Sushilkumar Zende <Sushilzende1@gmail.com>
2019-05-20 18:53:48 -0400

    Reverse SubString in a linked list

---
+++ Revision/src/problems/practice/ReverseSublist.java
@@ -0,0 +1,60 @@
+package problems.practice;
+
+/**
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ * 
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4 Output: 1->4->3->2->5->NULL
+ * 
+ * @author sushi
+ *
+ */
+public class ReverseSublist {
+
+	public static void main(String[] args) {
+
+	}
+
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+
+		if (head == null) {
+			return null;
+		}
+
+		ListNode prev = null;
+		ListNode curr = head;
+
+		while (m > 1) {
+			prev = curr;
+			curr = curr.next;
+			m--;
+			n--;
+		}
+
+		ListNode con = prev;
+		ListNode tail = curr;
+
+		ListNode third = null;
+
+		while (n > 0) {
+			third = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = third;
+			n--;
+		}
+
+		if (con != null) {
+			con.next = prev;
+		} else {
+			head = prev;
+		}
+
+		tail.next = curr;
+		return head;
+
+	}
+}
