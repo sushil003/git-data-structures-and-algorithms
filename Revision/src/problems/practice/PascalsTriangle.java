3f50bc4af914afe88eecfecfeb1b23f6691c2d06
Sushilkumar Zende <Sushilzende1@gmail.com>
2019-07-05 10:14:00 -0400

    Pascals traingle

---
+++ Revision/src/problems/practice/PascalsTriangle.java
@@ -0,0 +1,56 @@
+package problems.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+Example: Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ * @author Sushil
+ *
+ */
+public class PascalsTriangle {
+
+	public static void main(String[] args) {
+
+		System.out.println(generate(5));
+	}
+
+	public static List<List<Integer>> generate(int numRows) {
+
+		List<List<Integer>> results = new ArrayList<>();
+
+		if (numRows == 0) {
+			return results;
+		}
+
+		List<Integer> first = new ArrayList<>();
+		first.add(1);
+		results.add(first);
+
+		for (int i = 1; i < numRows; i++) {
+
+			List<Integer> currRow = new ArrayList<>();
+			List<Integer> prevRow = results.get(i - 1);
+			currRow.add(1);
+
+			for (int j = 1; j < i; j++) {
+
+				currRow.add(prevRow.get(j - 1) + prevRow.get(j));
+
+			}
+			currRow.add(1);
+			results.add(currRow);
+		}
+		return results;
+	}
+}
